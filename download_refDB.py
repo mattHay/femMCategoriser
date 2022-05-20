5dee787172abcac6e74b78aacf51b691d0fe64ca
Matthew R. Hayward <mrh24@compute-a-16-168.o2.rc.hms.harvard.edu>
2022-05-20 14:27:04 -0400

    downloads

--- download_refDB.py
+++ download_refDB.py
@@ -3,7 +3,7 @@
 import os
 
 print("downloading representative genomes")
-os.system("wget https://zenodo.org/record/6464921/files/genome_representatives.tar.gz")
+os.system("wget https://zenodo.org/record/6567596/files/genome_representatives.tar.gz")
 
 print("decompressing and unpacking")
 os.system("tar xfz genome_representatives.tar.gz")
